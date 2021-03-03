ee9ab7973f09e69b001caf47a0b77a30dbd3e98b
Enver-B <enverbelyalov86@gmail.com>
2021-03-03 16:39:32 +0300

    Задание 15. Доступная разметка главной страницы

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const letterLink = document.querySelector(".letter-link");
+const letterPopup = document.querySelector(".modal-window");
+const letterClose = letterPopup.querySelector(".modal-close");
+const letterForm = letterPopup.querySelector(".letter-form");
+const letterName = letterPopup.querySelector(".form-name-surname");
+const letterMail = letterPopup.querySelector(".form-mail");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("name")
+} catch (err) {
+  isStorageSupport = false;
+}
+
+letterLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  letterPopup.classList.add(".modal-show");
+
+  if (storage) {
+    letterName.value = storage;
+    letterMail.focus()
+  } else {
+    letterName.focus();
+  }
+})
+
+letterClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  letterPopup.classList.remove(".modal-show");
+})
+
+letterForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+})
+
+letterForm.addEventListener("submit", function (evt) {
+  if (!letterName.value || !letterMail.value) {
+    evt.preventDefault();
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", letterName.value);
+    }
+  }
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (letterPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      letterPopup.classList.remove("modal-show")
+    }
+  }
+});
